780e2bd497bfdc94c44d7924486246389da570fa
bergi <s.berke@netzkolchose.de>
2022-01-28 02:24:36 +0100

    add multi-threading support (and use in workflow)

--- update.py
+++ update.py
@@ -1,9 +1,10 @@
 import argparse
 import datetime
 import traceback
+from multiprocessing.pool import ThreadPool
 from typing import List
 
-from src.scraper import scraper_classes
+from src.scraper import Scraper, scraper_classes
 import src.sources
 
 
@@ -18,32 +19,51 @@ def parse_args() -> dict:
         "-v", "--verbose", type=bool, nargs="?", default=False, const=True,
         help="Print a lot of stuff"
     )
+    parser.add_argument(
+        "-j", "--threads", type=int, default=1,
+        help="Number of parallel threads (per scraper)"
+    )
 
     return vars(parser.parse_args())
 
 
-def main(filter: List[str], verbose: bool):
+def scrape(scraper: Scraper) -> str:
+    """
+    Run the scraper, return result message text
+    """
+    msg = f"### {scraper.NAME}\n\n"
+
+    try:
+        report = scraper.download()
+
+        for key, value in report.items():
+            if value:
+                msg += f"- {value} pages {key}\n"
+
+    except Exception as e:
+        msg += f"```\n{traceback.format_exc(limit=-2)}```"
+
+    return msg
+
+
+def main(filter: List[str], verbose: bool, threads: int):
 
     filtered_classes = []
     for name in sorted(scraper_classes.keys()):
         if not filter or name in filter:
             filtered_classes.append(scraper_classes[name])
 
-    print(f"update @ {datetime.datetime.utcnow().replace(microsecond=0)} UTC")
+    print(f"update @ {datetime.datetime.utcnow().replace(microsecond=0)} UTC\n")
 
-    for scraper_class in filtered_classes:
+    scrapers = [
+        scraper_class(verbose=verbose)
+        for scraper_class in filtered_classes
+    ]
 
-        scraper = scraper_class(verbose=verbose)
-        print(f"\n### {scraper.NAME}\n")
-        try:
-            report = scraper.download()
+    messages = ThreadPool(threads).map(scrape, scrapers)
+    messages.sort()
 
-            for key, value in report.items():
-                if value:
-                    print(f"- {value} pages {key}")
-
-        except Exception as e:
-            print(f"```\n{traceback.format_exc(limit=-2)}```")
+    print("\n".join(messages))
 
 
 if __name__ == "__main__":
